517e406b791ef37c0a4fbe8e757a47173f61f91b
SYERA AL-FARIDZI <syeraalfaridzi03@gmail.com>
2025-03-09 22:42:42 +0800

    feat: add about, contact, and tools pages with corresponding views and templates

--- porfolio/urls.py
+++ porfolio/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from porfolio.views import home
+from porfolio.views import home,about,contact,tools
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('about/',about , name='about'),
+    path('contact/', contact , name='contact'),
+    path('tools/', tools , name='tools'),
 ]
--- porfolio/views.py
+++ porfolio/views.py
@@ -3,8 +3,35 @@ from django.shortcuts import render
 def home(request):
     template_name = 'portfolio/home.html'
     context = {
-        'title': 'Syera Al-Faridzi',
-        'subtitle': 'Mau Jadi Software Engineer',
+        'title': 'Home',
+        'subtitle': 'Syera Al-Faridzi',
         'body': 'Halaman Utama'
+    }
+    return render(request, template_name, context)
+
+def about(request):
+    template_name = 'portfolio/about.html'
+    context = {
+        'title': 'About',
+        'subtitle': 'Syera Al-Faridzi',
+        'body': 'Halaman Tentang Saya'
+    }
+    return render(request, template_name, context)
+
+def contact(request):
+    template_name = 'portfolio/contact.html'
+    context = {
+        'title': 'Contact',
+        'subtitle': 'Syera Al-Faridzi',
+        'body': 'Halaman Kontak'
+    }
+    return render(request, template_name, context)
+
+def tools(request):
+    template_name = 'portfolio/tools.html'
+    context = {
+        'title': 'Tools',
+        'subtitle': 'Syera Al-Faridzi',
+        'body': 'Halaman Tools'
     }
     return render(request, template_name, context)
