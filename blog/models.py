1ad9d87c9a31402c5b5dde7eb15bfdb298596c18
alfrdzley <syeraalfaridzi03@gmail.com>
2025-03-15 01:45:07 +0800

    feat: create new app blog dan create model post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    publication_at = models.DateTimeField()
+    author = models.CharField(max_length=100)
+    status = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
