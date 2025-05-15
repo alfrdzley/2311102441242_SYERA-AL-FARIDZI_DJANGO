3c474ccd9e442b008630a12659f78bc00b223cfb
alfrdzley <syeraalfaridzi03@gmail.com>
2025-05-16 00:38:08 +0800

    feat: add login functionality and update base template structure

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import blog, dashboard, blog_detail
+from blog.views import blog, dashboard, blog_detail, blog_login
 
 urlpatterns = [
     path('', blog, name='blog'),
     path('dashboard', dashboard, name='dashboard'),
     path('detail/<slug:slug>/', blog_detail, name='blog_detail'),
+    path('login/', blog_login, name='login'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from pyexpat.errors import messages
+
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
 
+@login_required
 def dashboard(request):
     templates_name = "dashboard/index.html"
     context = {
@@ -28,3 +33,16 @@ def blog_detail(request, slug):
         'post': post,
     }
     return render(request, templates_name, context)
+
+
+def blog_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+    return render(request, 'blog/login.html')
