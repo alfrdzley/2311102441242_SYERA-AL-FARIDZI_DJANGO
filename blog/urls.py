4d18811d8502a60566e7f47b762a3abeb89cabc2
SYERA AL-FARIDZI <syeraalfaridzi03@gmail.com>
2025-05-27 02:39:06 +0800

    feat: add ckeditor

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import blog, dashboard, blog_detail, blog_login, blog_signup
+from blog.views import blog, dashboard, blog_detail, blog_login, blog_signup, logout_view
 
 urlpatterns = [
     path('', blog, name='blog'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('detail/<slug:slug>/', blog_detail, name='blog_detail'),
     path('login/', blog_login, name='login'),
     path('signup/', blog_signup, name='signup'),
+    path('logout/', logout_view, name='logout'),
 ]
